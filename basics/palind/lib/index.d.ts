b98ac5a5fc8fac711683bd287c1a8124e9b122d7
guifavere <guilherme.favere@gmail.com>
2021-12-21 16:03:27 -0300

    remove folder: lib

--- basics/palind/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function isPalindrome(str: string): boolean;
-//# sourceMappingURL=index.d.ts.map
--- basics/palind/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isPalindrome = void 0;
-function isPalindrome(str) {
-    return str === str.split('').reverse().join('');
-}
-exports.isPalindrome = isPalindrome;
-//# sourceMappingURL=index.js.map
